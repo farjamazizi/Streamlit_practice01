dd60d8352ee8e63bf446ec5b852ca568adcc0e87
farjamazizi <farjamazizi@gmail.com>
2021-12-22 20:26:17 +0330

    codying style

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import time
+
 import streamlit as st
+
 import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
@@ -49,9 +52,10 @@ st.write(prediction_proba)
 
 st.balloons()
 
-import time
+
 my_bar = st.progress(0)
 for percent_complete in range(100):
     time.sleep(0.1)
     my_bar.progress(percent_complete + 1)
+
 
